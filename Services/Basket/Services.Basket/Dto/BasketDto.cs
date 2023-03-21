3f8da3236fc07c4e5448e25c203f89504d2d0c19
Burak Ali ILDIR <burakaliildir@gmail.com>
2023-03-21 15:01:29 +0300

    Basket servis oluşturuldu.

---
+++ Services/Basket/Services.Basket/Dto/BasketDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Services.Basket.Dto
+{
+    public class BasketDto
+    {
+        public string UserId { get; set; }
+        public string DiscountCode { get; set; }
+        public List<BasketItemDto> BasketItems { get; set; }
+
+        public decimal TotalPrice
+        {
+            get => BasketItems.Sum(x => x.Price * x.Quantity);
+        }
+    }
+}
---
+++ Services/Basket/Services.Basket/Dto/BasketItemDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Services.Basket.Dto
+{
+    public class BasketItemDto
+    {
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
+        public uint Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
