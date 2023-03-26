529069a91173999d486d111f923004a4a421b7b1
Burak Ali ILDIR <burakaliildir@gmail.com>
2023-03-26 12:37:50 +0300

    Gateway API hata giderildi.

--- Gateway/Gateway/Program.cs
+++ Gateway/Gateway/Program.cs
@@ -5,11 +5,12 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json");
+//builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json");
+builder.Configuration.AddJsonFile($"ocelot.development.json");
 
 builder.Services.AddOcelot();
 
-builder.Services.AddAuthentication().AddJwtBearer(options =>
+builder.Services.AddAuthentication().AddJwtBearer("GatewayAuthSchema",options =>
 {
     options.TokenValidationParameters = new TokenValidationParameters()
     {
@@ -24,7 +25,10 @@ builder.Services.AddAuthentication().AddJwtBearer(options =>
 
 var app = builder.Build();
 
+app.UseAuthorization();
+
 app.UseOcelot();
+
 
 
 app.Run();
